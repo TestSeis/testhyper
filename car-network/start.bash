6638b74745b0ea68af589573ace04713c0cf77b7
TestSeis <45743627+TestSeis@users.noreply.github.com>
2019-01-18 20:31:03 +0100

    Update start.bash

--- car-network/start.bash
+++ car-network/start.bash
@@ -1,4 +1,4 @@
  #!/bin/bash
 composer archive create --sourceType dir --sourceName .
-composer network install --archiveFile car-network@1.0.6-deploy.32.bna --card PeerAdmin@hlfv1
-composer network start --networkName car-network --networkVersion 1.0.6-deploy.32 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file car-admin.card
+composer network install --archiveFile car-network@1.0.6-deploy.33.bna --card PeerAdmin@hlfv1
+composer network start --networkName car-network --networkVersion 1.0.6-deploy.33 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file car-admin.card
